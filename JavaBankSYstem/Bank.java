56c00cfd9edd76a25edb928847198c20538127a8
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2023-08-29 08:34:02 +0530

    Update Bank.java
    Complate Bank Management System - BruteForce Method

--- JavaBankSYstem/Bank.java
+++ JavaBankSYstem/Bank.java
@@ -1,63 +1,157 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Bank {
+public class Bank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    // Set All The Attribute
+        System.out.println("Enter How Much Coustomer You wnat to enter: ");
+        int n = sc.nextInt();
+        BankDetails C[] = new BankDetails[n];
+
+        for (int i = 0; i < C.length; i++) {
+            C[i] = new BankDetails();
+            C[i].createAccount();
+        }
+
+        // Loop untils 5 is Not Pressed To Exit The System.
+        int choice;
+        do {
+            System.out.println("\n ***Banking System Application***");
+            System.out.println(
+                    "1. Display all account details \n 2. Search by Account number\n 3. Deposit the amount \n 4. Withdraw the amount \n 5.Exit ");
+            System.out.println("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    for (int i = 0; i < C.length; i++) {
+                        C[i].showAccount();
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Enter Account No You want to Search: ");
+                    String accno = sc.next();
+                    boolean found = false;
+
+                    for (int i = 0; i < C.length; i++) {
+                        found = C[i].Search(accno);
+                        if (found) {
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Search Failed!\nAccount Desen't Exist!");
+                    }
+
+                case 3:
+                    System.out.println("Enter Your Account No: ");
+                    accno = sc.next();
+                    found = false;
+
+                    for (int i = 0; i < C.length; i++) {
+                        found = C[i].Search(accno);
+                        if (found) {
+                            C[i].diposite();
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Found Failed\nAccount Dosen't Exists!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Enter Your Account No: ");
+                    accno = sc.next();
+                    found = false;
+
+                    for (int i = 0; i < C.length; i++) {
+                        found = C[i].Search(accno);
+                        if (found) {
+                            C[i].withDraw();
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Found Failed\nAccount Dosen't Exists!");
+                    }
+
+                case 5:
+                    System.out.println("See You Soon!");
+
+                default:
+                    System.out.println("Invalid Input!!!");
+                    break;
+            }
+
+        } while (choice != 5);
+
+    }
+}
+
+class BankDetails {
     private String accno;
     private String name;
-    private String acc_type;
+    private String accType;
     private long balance;
 
-    // Create a Object for User input
+    // Creating a SCANNER Obj
     Scanner sc = new Scanner(System.in);
 
-    // 1. Method To Open a New Account
-    public Void openAccount() {
-        System.out.println("Enter Account No: ");
+    // 1. Method to Create an Account
+    public void createAccount() {
+        System.out.println("Enter Your Account No: ");
         accno = sc.next();
 
-        System.out.println("Enter Name: ");
+        System.out.println("Enter Your Name: ");
         name = sc.next();
 
-        System.out.println("Enter Account Type: ");
-        acc_type = sc.next();
+        System.out.println("Enter Your Account Type: ");
+        accType = sc.next();
 
-        System.out.println("Enter Balence: ");
+        System.out.println("Enter Your Balance: ");
         balance = sc.nextLong();
     }
 
-    // 2. Method To Display The account Details.
-
+    // 2. Method to Show The Account Details
     public void showAccount() {
-        System.out.println("Name of account Holder -> " + name);
-        System.out.println("Name of account NO -> " + accno);
-        System.out.println("Name of Account Type -> " + acc_type);
-        System.out.println("Balence -> " + balance);
+        System.out.println("The Name of The account Holder -> " + name);
+        System.out.println("The account Number -> " + accno);
+        System.out.println("The account Type -> " + accType);
+        System.out.println("The Balance is -> " + balance);
     }
 
-    // 3. Diposite Ammount
-    public void Diposit() {
-        long amt;
-        System.out.println("Enter The Amount You want to Diposite: ");
-        amt = sc.nextLong();
-        balance = balance + amt;
+    // 3. Method to Diposite Money
+    public void diposite() {
+        long amount;
+        System.out.println("Enter How Much Amount you want to add: ");
+        amount = sc.nextLong();
+        balance = balance + amount;
     }
 
-    // 4. Withdraw The Amount
-    public void withDraw(){
-        long amt;
-        System.out.println("Enter The Amount You want To withDraw: ");
-        amt = sc.nextLong();
+    // 4. Method to Withdraw The Amount
+    public void withDraw() {
+        long amount;
+        System.out.println("Enter How much Amount You want to Withdraw: ");
+        amount = sc.nextLong();
 
-        if(balance >= amt){
-            balance = balance - amt;
-            System.out.println("Balence After withdraw " + balance);
-        }else{
-            System.out.println("Your Account Balence is less " + amt + " Transition Failed!");
+        if (balance >= amount) {
+            balance = balance - amount;
+            System.out.println("Now Your Account Balance is ->" + balance);
+        } else {
+            System.out.println("OOp's Your Account Balance is less than " + amount + " Transition Failed!");
         }
     }
 
-    public static void main(String[] args) {
-
+    // 5. Method To Search The Account
+    public boolean Search(String accno) {
+        if (accno.equals(accno)) {
+            showAccount();
+            return (true);
+        }
+        return (false);
     }
+
 }
